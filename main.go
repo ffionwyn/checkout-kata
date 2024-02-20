d5fac62812dd9e5d8a5d3e37ff40a6d5c048f9e1
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2024-02-20 18:51:06 +0000

    structs
    going to use a map of structs to lookup/keep track of quantities for the checkout system

--- main.go
+++ main.go
@@ -1,2 +1,21 @@
 package main
 
+// item struct to represent each item.
+type Item struct {
+    Name         string
+    UnitPrice    int
+    SpecialPrice SpecialPrice
+}
+
+// special price struct to represent special pricing for an item.
+type SpecialPrice struct {
+    Quantity int
+    Price    int
+}
+
+// checkout struct to represent the checkout system.
+type Checkout struct {
+    items map[string]Item
+    cart  map[string]int
+}
+
